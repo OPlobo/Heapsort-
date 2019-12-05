10354fa49d1f0f79745ab25f48d3d869597ff8f8
OPlobo <55872350+OPlobo@users.noreply.github.com>
2019-12-05 23:05:41 +0800

    Update hash_table_05131033.py

--- HW4/hash_table_05131033.py
+++ HW4/hash_table_05131033.py
@@ -4,19 +4,18 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-        
+
 class MyHashSet:
     def __init__(self, capacity=5):
         self.capacity = capacity
         self.data = [None] * capacity
-        
+
     def encrypt(self,key):
         h=MD5.new()
         h.update(key.encode("utf-8"))
         x=h.hexdigest()
         x=int(h.hexdigest(),16)
         return x
-    
     def add(self, key):
         x=self.encrypt(key)
         y=x%self.capacity
@@ -24,18 +23,30 @@ class MyHashSet:
         if node:
             if node.val==key:
                 return
-            node=node.next
-            newnode=ListNode(key)
-            self.data[y].next=newnode
+            else:
+                node=node.next
+                newnode=ListNode(key)
+                self.data[y].next=newnode
         else:
             newnode=ListNode(key)
             self.data[y]=newnode
-            
+
     def remove(self, key):
-        """
-        :type key: str
-        :rtype: None
-        """
+        x=self.encrypt(key)
+        y=x%self.capacity
+        node=self.data[y]
+        if node.val==key:
+            self.data[y]=node.next
+            return None
+        i=None
+        while node:
+            if node.val==key:
+                i.next=node.next
+                return None
+            else:
+                i=node
+                node=node.next
+
     def contains(self, key):
         x=self.encrypt(key)
         y=x%self.capacity
@@ -43,6 +54,6 @@ class MyHashSet:
         while node:
             if node.val==key:
                 return True
-            node=node.next
+            else:
+                node=node.next
         return False
-
