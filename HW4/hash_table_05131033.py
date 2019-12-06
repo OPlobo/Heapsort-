f474e60ed33432c175ce840248c2e072188bef8f
OPlobo <55872350+OPlobo@users.noreply.github.com>
2019-12-06 23:17:29 +0800

    Update hash_table_05131033.py

--- HW4/hash_table_05131033.py
+++ HW4/hash_table_05131033.py
@@ -10,27 +10,29 @@ class MyHashSet:
         self.capacity = capacity
         self.data = [None] * capacity
 
-    def encrypt(self,key):
+    def encrypt(self,key):                              #加密
         h=MD5.new()
         h.update(key.encode("utf-8"))
         x=h.hexdigest()
         x=int(h.hexdigest(),16)
         return x
+    
+    def get(self,node,key):                            #增加並確認
+        if node.next:
+            return self.get(node.next,key)
+        else:
+            node.next=ListNode(key)
+            
     def add(self, key):
         x=self.encrypt(key)
         y=x%self.capacity
-        node=self.data[y]
-        if node:
-            if node.val==key:
-                return
-            else:
-                node=node.next
-                newnode=ListNode(key)
-                self.data[y].next=newnode
-        else:
+        print(y)
+        if self.data[y]==None:                        #若一開始為空，則直接加入
             newnode=ListNode(key)
             self.data[y]=newnode
-
+        else:
+            newnode=self.get(self.data[y],key)
+            
     def remove(self, key):
         x=self.encrypt(key)
         y=x%self.capacity
@@ -47,15 +49,16 @@ class MyHashSet:
                 i=node
                 node=node.next
 
-    def contains(self, key):
+    def contains(self, key):                         #是否存在
         x=self.encrypt(key)
         y=x%self.capacity
+        print(y)
         node=self.data[y]
         while node:
-            if node.val==key:
+            if node.val==key:                       #如果再，就回傳True
                 return True
             else:
-                node=node.next
-        return False
+                node=node.next                      #如果不再，就往下找
+        return False                                #不存在，則回傳False
 
     #參考資料:老師課堂投影片
