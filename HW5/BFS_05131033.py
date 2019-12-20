11e36de4f1784d3c688d146dbb148a1e90434bf2
OPlobo <55872350+OPlobo@users.noreply.github.com>
2019-12-20 23:55:01 +0800

    Update BFS_05131033.py

--- HW5/BFS_05131033.py
+++ HW5/BFS_05131033.py
@@ -12,16 +12,16 @@ class Graph:
         q2=[]
         if s:
             q2.append(s)
-            a=self.graph[s]
+            a=self.graph[s]                        
             while a:
-                b=a.pop(0)
+                b=a.pop(0)                        
                 q2.append(b)
                 p=self.graph[b]
-                for i in range(0,len(p)):
+                for i in range(0,len(p)):   #走訪下一個連結
                     x=g.graph[b][i]
-                    if x not in q2:
-                        if x not in a:
-                            a.append(x)
+                    if x not in q2:         #如果不在q2
+                        if x not in a:      #如果不在a
+                            a.append(x)     #則加入
         return q2
 
     def DFS(self, s):
@@ -29,4 +29,4 @@ class Graph:
         :type s:int
         :rtype: list
         """
-#參考資料:
+#參考資料:老師課堂投影片
